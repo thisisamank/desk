7145723c88cb2953978408b07bac969c5434d3ce
Aman Kumar <thisisamank@gmail.com>
2024-06-16 17:06:11 +0530

    chore: Add debug and warning logging methods to DeskLogger

--- server/app/logger.py
+++ server/app/logger.py
@@ -24,6 +24,12 @@ class DeskLogger:
     def info(self,msg: str):
         self.logger.info(msg=msg),
     
+    def debug(self, msg: str):
+        self.logger.debug(msg)
+    
+    def warning(self, msg: str):
+        self.logger.warning(msg)
+    
     def error(self, msg: str):
         self.logger.error(msg)
 
