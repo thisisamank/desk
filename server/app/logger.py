bfbda1419efac0682b9fd5ec4b6c2a2b43134e8d
Aman Kumar <thisisamank@gmail.com>
2024-06-16 16:29:44 +0530

    feat: Add CORS middleware and logging to main.py

---
+++ server/app/logger.py
@@ -0,0 +1,23 @@
+import logging
+import json
+
+
+
+class DeskLogger:
+
+    def __init__(self) -> None:
+        self.formatter = logging.Formatter(
+    "%(levelname)s: %(asctime)s - %(message)s",datefmt="%Y-%m-%d %H:%M:%S"
+)
+        self.handler = logging.StreamHandler()
+        self.logger = logging.getLogger(__name__)
+        self.handler.setFormatter(self.formatter)
+        self.logger.setLevel(logging.INFO)
+        self.logger.addHandler(self.handler)
+
+
+    def info(self,msg: str):
+        self.logger.info(msg=msg)
+    
+    def error(self, msg: str):
+        self.logger.error(msg, exc_info=True)
--- server/main.py
+++ server/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from app.api.controller.course_controller import CourseController
-from typing import List
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-
+from fastapi.middleware.cors import CORSMiddleware
+from app.logger import DeskLogger 
 
 app = FastAPI()
+logger = DeskLogger()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 course_controller = CourseController()
 
@@ -18,11 +27,13 @@ def ping():
 def add_course(path: str):
     course = course_controller.add_course(path)
     course_json = jsonable_encoder(course)
+    logger.info(msg=f"Course added: {course_json}")
     return JSONResponse(content=course_json)
 
 
 @app.get('/course')
 def get_all_courses():
+    logger.info(msg="Getting all courses")
     return course_controller.get_all_courses()
 
 @app.get('/course/:id')
