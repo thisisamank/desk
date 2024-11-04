06cfa4798d6a685c374a374ef66528249b184abc
Aman Kumar <thisisamank@gmail.com>
2024-11-04 23:09:04 +0530

    feat: add extension support to content type

--- desk-ui/src/app/api/videos/route.ts
+++ desk-ui/src/app/api/videos/route.ts
@@ -1,11 +1,19 @@
-import { NextRequest, NextResponse } from 'next/server';
 import fs from 'fs';
+import { NextRequest, NextResponse } from 'next/server';
 import path from 'path';
+
+function generateContentType(videoPath: string){
+  const filePathParts = videoPath.split(".")
+  const extension = filePathParts[filePathParts.length - 1]
+  return extension
+}
 
 export async function POST(req: NextRequest) {
   try {
     const { videoPath } = await req.json();
     console.log(videoPath);
+    generateContentType(videoPath)
+
 
     if (!fs.existsSync(videoPath)) {
       return new NextResponse('Video not found', { status: 404 });
@@ -15,6 +23,8 @@ export async function POST(req: NextRequest) {
     const stat = fs.statSync(videoFilePath);
     const fileSize = stat.size;
     const range = req.headers.get('range');
+    const extension = generateContentType(videoFilePath)
+
 
     if (range) {
       const parts = range.replace(/bytes=/, "").split("-");
@@ -26,13 +36,13 @@ export async function POST(req: NextRequest) {
         'Content-Range': `bytes ${start}-${end}/${fileSize}`,
         'Accept-Ranges': 'bytes',
         'Content-Length': chunksize.toString(),
-        'Content-Type': 'video/mp4',
+        'Content-Type': `video/${extension}`,
       };
       return new NextResponse(file as any, { status: 206, headers: head });
     } else {
       const head = {
         'Content-Length': fileSize.toString(), 
-        'Content-Type': 'video/mp4',
+        'Content-Type': `video/${extension}`,
       };
       const file = fs.createReadStream(videoFilePath);
       return new NextResponse(file as any, { status: 200, headers: head });
